eced4ff2607e07ac613ce69a90c2b7835c42575e
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-07-18 21:13:50 -0600

    Usuarios con rutas

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -1,75 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
-import { UserComponent } from './user/user.component';
-import { UserFormComponent } from './user-form/user-form.component';
 import Swal from 'sweetalert2';
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SharingDataService } from '../services/sharing-data.service';
 
 @Component({
   selector: 'user-app',
   standalone: true,
-  imports: [UserComponent, UserFormComponent],
-  templateUrl: './user-app.component.html',
-  styleUrls: ['./user-app.component.css']
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './user-app.component.html'
 })
 export class UserAppComponent implements OnInit {
-  title: string = 'Listado de usuarios!';
+  
 
   users: User[] = [];
 
-  userSelected: User;
+  constructor(
+    private router: Router,
+    private service: UserService,
+    private sharingData: SharingDataService
+  ){}
 
-  open: boolean = false;
-
-  constructor(private service: UserService){
-    this.userSelected = new User();
-  }
   ngOnInit(): void {
     this.service.findAll().subscribe( users => this.users = users);
+    this.addUser();
+    this.removeUser();
   }
 
-  addUser(user: User){
-    if(user.id > 0){
-      this.users = this.users.map( u => (u.id == user.id) ? { ... user} : u);
-    } else {
-      this.users = [... this.users, { ... user, id: new Date().getTime() }];
-    }
-    Swal.fire({
-      title: "Guardado!",
-      text: "Usuario guardado con éxito!",
-      icon: "success"
-    });
-    this.userSelected = new User();
-    this.setOpen();
-  }
-
-  removeUser(id: number): void {
-    Swal.fire({
-      title: "Seguro que quiere eliminar?",
-      text: "Cuidado, el usuario será eliminado del sistema!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Si!"
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.users = this.users.filter(user => user.id != id);
-        Swal.fire({
-          title: "Eliminado!",
-          text: "Usuario eliminado con exito",
-          icon: "success"
-        });
+  addUser(){
+    this.sharingData.newUserEventEmitter.subscribe(user => {
+      if(user.id > 0){
+        this.users = this.users.map( u => (u.id == user.id) ? { ... user} : u);
+      } else {
+        this.users = [... this.users, { ... user, id: new Date().getTime() }];
       }
+      this.router.navigate(['/users'], { state: { users: this.users }});
+      Swal.fire({
+        title: "Guardado!",
+        text: "Usuario guardado con éxito!",
+        icon: "success"
+      });
+    })
+  }
+
+  removeUser(): void {
+    this.sharingData.idUserEventEmitter.subscribe(id => {
+      Swal.fire({
+        title: "Seguro que quiere eliminar?",
+        text: "Cuidado, el usuario será eliminado del sistema!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.users = this.users.filter(user => user.id != id);
+          this.router.navigate(['/users/create'], {skipLocationChange: true}).then(() => {
+            this.router.navigate(['/users'], { state: { users: this.users }});
+          })
+          Swal.fire({
+            title: "Eliminado!",
+            text: "Usuario eliminado con exito",
+            icon: "success"
+          });
+        }
+      });
     });
-  }
-
-  setSelectedUser(userRow: User): void{
-    this.userSelected = {... userRow};
-    this.open = true;
-  }
-
-  setOpen(){
-    this.open = !this.open;
   }
 }
